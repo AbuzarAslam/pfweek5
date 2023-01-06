7df6dfa54eb1becbebd424441d1668e0e47bc5e5
AbuzarAslam <120781089+AbuzarAslam@users.noreply.github.com>
2023-01-06 06:24:26 -0800

    Add files via upload

---
+++ task6.cpp
@@ -0,0 +1,161 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int num;
+    int modulus;
+    int remainder;
+    cout << "Enter number...";
+    cin >> num;
+    if(num==1)
+    {
+        cout << "One";
+    }
+    if(num==2)
+    {
+        cout << "Two";
+    }
+    if(num==3)
+    {
+        cout << "Three";
+    }
+    if(num==4)
+    {
+        cout << "four";
+    }
+    if(num==5)
+    {
+        cout << "five";
+    }
+    if(num==6)
+    {
+        cout << "six";
+    }
+    if(num==7)
+    {
+        cout << "seven";
+    }
+    if(num==8)
+    {
+        cout << "Eight";
+    }
+    if(num==9)
+    {
+        cout << "Nine";
+    }
+    if(num==10)
+    {
+        cout << "Ten";
+    }
+    if(num==11)
+    {
+        cout << "Eleven";
+    }
+    if(num==12)
+    {
+        cout << "Twelve";
+    }
+    if(num==13)
+    {
+        cout << "thirteen";
+    }
+    if(num==14)
+    {
+        cout << "Fourteen";
+    }
+    if(num==15)
+    {
+        cout << "Fifteen";
+    }
+    if(num==16)
+    {
+        cout << "sixteen";
+    }
+     if(num==17)
+    {
+         cout << "Seventeen";
+    }
+    if(num==18)
+    {
+        cout << "Eighteen";
+    }
+    if(num==19)
+    {
+        cout << "Nineteen";
+    }
+   if(num==20)
+    {
+        cout << "Twenty";
+    }
+    modulus = num %10;
+    remainder = num /10;
+    if (remainder == 2)
+    {
+        cout << "Twenty" ;
+    }
+    if (remainder == 3)
+    {
+        cout << "Thirty" ;
+    }
+    if (remainder == 4)
+    {
+        cout << "Forty" ;
+    } 
+   if (remainder == 5)
+    {
+        cout << "Fifty" ;
+    }
+    if (remainder == 6)
+    {
+        cout << "Sixty" ;
+    } 
+    if (remainder == 7)
+    {
+        cout << "Seventy" ;
+    }
+    if (remainder == 8)
+    {
+        cout << "Eighty" ;
+    }
+    if (remainder == 9)
+    {
+        cout << "Ninty" ;
+    }
+    if (modulus == 1)
+    {
+        cout << "One" ;
+    }
+    if(modulus == 2)
+    {
+        cout << "Two" ;
+    }
+    if(modulus == 3)
+    {
+        cout << "Three" ;
+    }
+    if(modulus == 4)
+    {
+        cout << "Four" ;
+    }
+    if(modulus == 5)
+    {
+         cout << "Five" ;
+    }
+    if(modulus == 6)
+    {
+        cout << "Six" ;
+    }
+    if(modulus == 7)
+    {
+        cout << "Seven" ;
+    }
+    if(modulus == 8)
+    {
+        cout << "Eight" ;
+    }
+    if(modulus == 9)
+    {
+        cout << "Nine" ;
+    }
+    
+}
